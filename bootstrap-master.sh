6595ce259164ea028785544390c8be4a7e700285
Leon Waldman <le.waldman@gmail.com>
2014-03-25 01:25:19 -0300

    Removed unneeded lock file instruction

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-# Do not run twice
-if [ ! -f vagrant_bootstrap_master ]; then
-    # Install curl
-    apt-get -y install curl
+# Install curl
+apt-get -y install curl
 
-    # Installs salt master
-    curl -L http://bootstrap.saltstack.org | sudo sh -s -- -M -N
+# Installs salt master
+curl -L http://bootstrap.saltstack.org | sudo sh -s -- -M -N
 
-    # Removing salt configs and linking from synced folder copy
-    rm /etc/salt -rf
-    ln -fs /vagrant/master/etc /etc/salt
+# Removing salt configs and linking from synced folder copy
+rm /etc/salt -rf
+ln -fs /vagrant/master/etc /etc/salt
 
-    # Setting up the roots base folder
-    ln -fs /vagrant/master/roots /srv/salt
+# Setting up the roots base folder
+ln -fs /vagrant/master/roots /srv/salt
 
-    # Master restart
-    service salt-master restart
-    sleep 2
-    service salt-minion restart
-fi
-
-touch vagrant_bootstrap_master
+# Master restart
+service salt-master restart
+sleep 2
+service salt-minion restart
--- bootstrap-webapp.sh
+++ bootstrap-webapp.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# Do not run twice
-if [ ! -f vagrant_bootstrap_webapp ]; then
-    # Setting up the webapp symlink
-    ln -fs /vagrant/webapp /srv/webapp
+# Setting up the webapp symlink
+ln -fs /vagrant/webapp /srv/webapp
 
-    # Running salt highstate
-    salt-call state.highstate
-fi
-
-touch vagrant_bootstrap_webapp
+# Running salt highstate
+salt-call state.highstate
